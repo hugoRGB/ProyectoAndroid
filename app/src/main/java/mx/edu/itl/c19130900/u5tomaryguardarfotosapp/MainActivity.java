f032d955a9e2e07f1419a23f734a07e94ca10e13
hugoRGB <huguerrabarajas@gmail.com>
2022-12-11 19:51:34 -0600

    Ya abre la app

--- app/src/main/java/mx/edu/itl/c19130900/u5tomaryguardarfotosapp/MainActivity.java
+++ app/src/main/java/mx/edu/itl/c19130900/u5tomaryguardarfotosapp/MainActivity.java
@@ -1,13 +1,9 @@
 package mx.edu.itl.c19130900.u5tomaryguardarfotosapp;
 
-import static android.os.Environment.getExternalStorageDirectory;
-
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,18 +14,15 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.BitSet;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button camara;
+    Button btnCamara;
     RadioButton escuela, fiesta, deportes, comida, familia, selfie;
     ImageView imgView;
     int CODIGO_ABRIR_CAMARA = 1;
@@ -46,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
         comida = findViewById(R.id.radioComida);
         familia = findViewById(R.id.radioFamilia);
         selfie = findViewById(R.id.radioSelfie);
+
+        btnCamara = findViewById(R.id.btnCamara);
         imgView = findViewById(R.id.imageView);
 
-        camara.setOnClickListener(new View.OnClickListener() {
+        btnCamara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirCamara();
@@ -58,18 +53,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void abrirCamara(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            File imagenArchivo = null;
-            try {
-                imagenArchivo = crearImagen();
-            } catch (IOException e){
-                Log.e("Error", e.toString());
-            }
+        File imagenArchivo = null;
+        try {
+            imagenArchivo = crearImagen();
+        } catch (IOException e){
+            Log.e("Error", e.toString());
+        }
 
-            if (imagenArchivo != null){
-                Uri fotoUri = FileProvider.getUriForFile(this, "mx.edu.itl.c19130900.u5tomaryguardarfotosapp.fileprovider",imagenArchivo);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,fotoUri);
-                startActivityForResult(intent, CODIGO_ABRIR_CAMARA);
-            }
+        if (imagenArchivo != null){
+            Uri fotoUri = FileProvider.getUriForFile(this, "mx.edu.itl.c19130900.u5tomaryguardarfotosapp.fileprovider",imagenArchivo);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,fotoUri);
+            startActivityForResult(intent, CODIGO_ABRIR_CAMARA);
+        }
     }
 
     @Override
